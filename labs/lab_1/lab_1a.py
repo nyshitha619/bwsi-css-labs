cccb3a220027a99e87ed47c349e0ca8b23adf129
nyshitha619 <nyshithaa@gmail.com>
2026-03-19 10:03:12 -0500

    Update lab_1a.py
    Added personal description to file

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Nyshitha A." # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("I am 16 years old from Texas. I hope to learn new skills from this course and have fun! :)")
 
 if __name__ == "__main__":
     main()

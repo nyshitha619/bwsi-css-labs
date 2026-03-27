29411a8065c479bb5d63fc0c04669ebfeb516519
nyshitha619 <nyshithaa@gmail.com>
2026-03-26 21:36:47 -0500

    sanitize number and operation
    fixed error from wrong number or math operator input from user

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -22,28 +22,37 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     Returns:
         float: The result of the operation.
     """
-
-    if operation == "add":
-        return num1 + num2
-    elif operation == "subtract":
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
+    try:
+        if operation == "add":
+            return num1 + num2
+        elif operation == "subtract":
+            return num1 - num2
+        elif operation == "multiply":
+            return num1 * num2
+        elif operation == "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                raise ValueError("Cannot divide by zero.")
         else:
-            raise ValueError("Cannot divide by zero.")
-    else:
-        raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+            raise ValueError("Invalid operation.")
+    except ValueError:
+        print("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+        main()
+def number_input(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = number_input("Enter the first number: ")
+    num2 = number_input("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
